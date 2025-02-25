107132eba5023460175beb9a34fc89af88df364f
Fxdzie <manwafadziso@gmail.com>
2025-02-25 15:50:10 +0100

    Added functionality to change layout from user input

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const container = document.querySelector(".container");
+const layoutButton = document.querySelector(".layout-change");
 
 const layout = (numSide)=>{
     for(let i = 0; i < numSide; i++){
@@ -16,6 +17,16 @@ const layout = (numSide)=>{
 }
 
 layout(16);
+layoutButton.addEventListener("click",()=>{
+    let num = prompt("How many squares per side would you like? (Max: 100)");
+    while(num > 100){
+        num = prompt("Oops, too many too handle. Try lower.");
+    }
+    while(container.firstChild){
+        container.removeChild(container.firstChild);
+    }
+    layout(num);
+});
 
 
 
