2d9c14a4a319caaf7ab713dd0c2f7a8aef21943a
Fxdzie <manwafadziso@gmail.com>
2025-02-25 15:33:09 +0100

    Added functionality to color in boxes when mouse hovers

--- script.js
+++ script.js
@@ -1,20 +1,21 @@
 const container = document.querySelector(".container");
-const defaultLayout = document.createElement("div");
 
 const layout = (numSide)=>{
     for(let i = 0; i < numSide; i++){
         container.appendChild(document.createElement("div"));
-        // for(let j =0 ;j <numSide; i++){
-            
-        // }
     }
 
     const rows = document.querySelectorAll(".container > div");
     rows.forEach((row)=>{
         for(let i=0;i<numSide;i++){
-            row.appendChild(document.createElement("div"));
+            const temp = document.createElement("div");
+            temp.addEventListener("mouseover",()=>{temp.style.backgroundColor="pink"})
+            row.appendChild(temp);
         }
     });
 }
 
-layout(64);
+layout(16);
+
+
+
